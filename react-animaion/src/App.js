d8d3f0e16f9b086d5777e9e3c802eff666da311e
Aleksei_Potapov <aleksei_potapov@epam.com>
2020-09-10 13:04:50 +0200

    add animation group example

--- react-animaion/src/App.js
+++ react-animaion/src/App.js
@@ -1,17 +1,30 @@
 import React, {useState} from 'react';
 import {Transition, CSSTransition} from "react-transition-group";
+import {List} from "./List";
 
 function App() {
 
     const [toggle, setToggle] = useState(true)
     const [toggle2, setToggle2] = useState(true)
+    const [items, setItems] = useState([
+        {id: 1, title: 'Item 1'},
+        {id: 2, title: 'Item 2'},
+        {id: 3, title: 'Item 3'},
+        {id: 4, title: 'Item 4'}
+    ])
+
+    const removeItem = id => setItems(items.filter(i => i.id !== id))
+    const addItem = () => {
+        const title = prompt('Enter item title')
+        const id = Date.now()
+        setItems(items.concat([{title, id}]))
+    }
 
     return (
         <div className="container">
-            <button onClick={() => setToggle(!toggle)}>Toggle
-            </button>
-            <button onClick={() => setToggle2(!toggle2)}>Toggle2
-            </button>
+            <button onClick={() => setToggle(!toggle)}>Toggle</button>
+            <button onClick={() => setToggle2(!toggle2)}>Toggle2</button>
+            <button onClick={addItem}>Add item</button>
             <hr/>
             <div className='blocks'>
                 <Transition
@@ -39,6 +52,10 @@ function App() {
                 >
                     <div className='square orange'>{toggle2.toString()}</div>
                 </CSSTransition>
+            </div>
+            <hr/>
+            <div className="blocks">
+                <List items={items} onRemove={removeItem}/>
             </div>
         </div>
     );
