429457907cfa34a38d781f8ef3792a6a80bf4670
Aleksei_Potapov <aleksei_potapov@epam.com>
2020-09-11 12:12:58 +0200

    add character component

--- react-unit-testing/src/App.js
+++ react-unit-testing/src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {Character} from "./Character";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+const App = ({side}) => {
+    if (!side) {
+        side = 'light'
+    }
+
+    const characters = [
+        {name: 'Люк Скайвокер', side: 'light'},
+        {name: 'Йода', side: 'light'},
+        {name: 'Обиван Кеноби', side: 'light'},
+        {name: 'Паооатин', side: 'dark'},
+        {name: 'Дарк Вэйдер', side: 'dark'},
+    ]
+
+    const filteredChars = characters.filter(char => char.side === side)
+    return (
+        <ul>
+            {filteredChars.map((char, index) => (
+                <Character key={char.name + index} name={char.name} side={char.side}/>
+            ))}
+        </ul>
+    );
 }
 
 export default App;
---
+++ react-unit-testing/src/Character.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Character = ({name, side}) =>
+    (<li>
+        {name} - <strong>{side}</strong>
+    </li>)
+
+
